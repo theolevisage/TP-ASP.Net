85e1fd0a043499fb55df33ae71b768960e10c070
Theo LE VISAGE <theo.levisage2019@campus-eni.fr>
2020-09-16 15:27:22 +0200

    wololo

---
+++ WebApplication1/Controllers/ChatController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPModule3.Database;
+
+namespace WebApplication1.Controllers
+{
+    public class ChatController : Controller
+    {
+        
+        // GET: Chat
+        public ActionResult Index()
+        {
+            
+            return View(FakeDb.Instance.ListeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var item = FakeDb.Instance.ListeChats.FirstOrDefault(x => x.Id == id);
+          
+            return View(item);
+        }
+
+        //// GET: Chat/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Chat/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Chat/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: Chat/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var meuteDeChats = FakeDb.Instance.ListeChats;
+            var item = meuteDeChats.FirstOrDefault(x => x.Id == id);
+            meuteDeChats.Remove(item);
+
+            return View(item);
+
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Database/FakeDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.Models;
+
+namespace TPModule3.Database
+{
+    public class FakeDb
+    {
+        private static readonly Lazy<FakeDb> lazy =
+            new Lazy<FakeDb>(() => new FakeDb());
+
+        /// <summary>
+        /// FakeDb singleton access.
+        /// </summary>
+        public static FakeDb Instance { get { return lazy.Value; } }
+
+        private FakeDb()
+        {
+            this.ListeChats = new List<Chat>();
+            this.InitialiserDatas();
+        }
+
+        public List<Chat> ListeChats { get; private set; }
+       
+
+        private void InitialiserDatas()
+        {
+            ListeChats = Chat.GetMeuteDeChats();
+        }
+    }
+}
